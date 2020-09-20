a6e626ab07ab77089a7915643c0ef01f856c6195
shaomin4896 <sam880819@gmail.com>
2020-09-21 04:13:28 +0800

    first

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+# add pods for any other desired Firebase products
+# https://firebase.google.com/docs/ios/setup#available-pods
+
+target 'Siri' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  
+  # Pods for Siri
+  pod 'Firebase/Firestore'  
+  pod 'GoogleMaps'
+  pod 'GooglePlaces'
+  pod 'Geofirestore'
+  pod 'LFLiveKit'
+  pod 'Socket.IO-Client-Swift'
+end 
---
+++ Siri/Controllers/MonitorController.swift
@@ -0,0 +1,30 @@
+//
+//  MonitorController.swift
+//  Siri
+//
+//  Created by 曾雅芳 on 2020/9/21.
+//  Copyright © 2020 Sahand Edrisian. All rights reserved.
+//
+
+import UIKit
+
+class MonitorController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Monitor did loaded")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
